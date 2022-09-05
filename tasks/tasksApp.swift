58e8fde1c9aa1bb1970612527f620a6bd22b0bc6
akp10110 <akp10110@gmail.com>
2022-09-05 20:55:27 +1000

    Initial Commit

---
+++ tasks/tasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  tasksApp.swift
+//  tasks
+//
+//  Created by Remya Pillai on 5/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct tasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
